ff63748ddf7004be794a0b338e4de323f10493e0
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-10-10 14:32:12 -0300

    Update Triangulo.cs

--- Testando13/Triangulo.cs
+++ Testando13/Triangulo.cs
@@ -1,7 +1,12 @@
-﻿namespace Testando13 {
+namespace Testando13 {
     internal class Triangulo {
         public double A;
         public double B;
         public double C;
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return System.Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
